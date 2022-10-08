5eb9021b668cbe578e3e85c4598524704fe2278b
Priyanshusinhaa <thepriyanshusinha@gmail.com>
2022-10-08 14:23:41 +0530

    done with circuit1 draw function

--- Circuit1/circuit1.py
+++ Circuit1/circuit1.py
@@ -1,16 +1,26 @@
 from qiskit import *
 import qiskit
 
+theta = [i for i in range(50)]
 class Circuit1:
-    def __init__(self, qubits, layers, thetaList) -> None:
+    def __init__(self, qubits, layers, thetaList, measure = False) -> None:
         self.qubits = qubits
         self.layers = layers
         self.thetaList = thetaList
-
-    def draw(self):
-        qc = QuantumCircuit(self.qubits)
+        self.qc = QuantumCircuit(self.qubits)
         for l in range(self.layers):
-            for qb in range(self.qubits):
-                qc.rx(thetaList[i + self.qubits])
-                qc.ry()
+            for q in range(0, self.qubits):
+                self.qc.rx(self.thetaList[2*l + q*self.layers*2], q)
+                self.qc.rz(self.thetaList[2*l+1 + q*self.layers*2], q)
+        if measure == True:
+            self.qc.measure_all()
+    def draw(self):
+        print(self.qc)
+    # def prob(self):
 
+
+
+
+Circuit1(4, 4, theta).draw()
+
+                
