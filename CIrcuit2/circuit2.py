b060c489ea7b760a1b78fcf4b4a14ba834e9408a
Priyanshusinhaa <thepriyanshusinha@gmail.com>
2022-10-08 16:39:50 +0530

    Added circuit2 repo

--- CIrcuit2/circuit2.py
+++ CIrcuit2/circuit2.py
@@ -2,28 +2,32 @@ from qiskit import *
 import qiskit
 
 theta = [i for i in range(50)]
-class Circuit1:
-    def __init__(self, qubits, layers, thetaList, measure = False) -> None:
+
+class circuit2:
+    def __init__(self, qubits, layers, thetaList) -> None:
         self.qubits = qubits
         self.layers = layers
         self.thetaList = thetaList
         self.qc = QuantumCircuit(self.qubits)
         for l in range(self.layers):
-            for q in range(0, self.qubits):
-                self.qc.rx(self.thetaList[2*l + q*self.layers*2], q)
-                self.qc.rz(self.thetaList[2*l+1 + q*self.layers*2], q)
-        if measure == True:
-            self.qc.measure_all()
+            for q in range(self.qubits):
+                self.qc.rx(self.thetaList[2*l + self.qubits*q*2], q)
+                self.qc.rz(self.thetaList[2*l+1 + self.qubits*q*2], q)
+            ent = self.qubits-1
+            while ent > 0:
+                self.qc.cx(ent, ent-1)
+                ent = ent -1
+
     def draw(self):
         print(self.qc)
-        
-    def statevector(self, type = None):
+    
+    def statevector(self):
         simu_sv = BasicAer.get_backend('statevector_simulator')
         sv = execute(self.qc, simu_sv).result().get_statevector()
         if type == 'print':
             print(sv)
         return sv
-
+    
     def counts(self, type = None):
         simu_sv = BasicAer.get_backend('statevector_simulator')
         counts = execute(self.qc, simu_sv).result().get_counts()
@@ -32,6 +36,4 @@ class Circuit1:
         return counts
 
 
-Circuit1(4, 4, theta).statevector(type = 'print')
-
-                
+circuit2(4, 6, theta).counts(type='print')
---
+++ try.py
@@ -0,0 +1,6 @@
+
+
+ent = 5
+while ent > 0:
+    print(ent)
+    ent = ent -1
