20e6f215005907f90550c5a7452f4bcad7a4ad3c
Priyanshusinhaa <thepriyanshusinha@gmail.com>
2022-10-10 22:41:55 +0530

    Done with circuit5

--- Circuit5/circuit5.py
+++ Circuit5/circuit5.py
@@ -5,52 +5,55 @@ theta = [i for i in range(100)]
 rcxList = [i for i in range(80)]
 
 class Circuit5:
-    def __init__(self, qubits, layers, thetaList, crxList) -> None:
+    def __init__(self, qubits, thetaList, crxList) -> None:
         self.qubits = qubits
-        self.layers = layers
         self.thetaList = thetaList
         self.crxList = iter(crxList)
         self.qc = QuantumCircuit(qubits)
-        for l in range(self.layers):
-            for q in range(self.qubits):
-                self.qc.rx(self.thetaList[2*l + self.qubits*q*2], q)
-                self.qc.rz(self.thetaList[2*l+1 + self.qubits*q*2], q)
-            self.qc.barrier()
+        self.qc2 = QuantumCircuit(qubits)
+        a = 0
+        for q in range(self.qubits):
+            self.qc.rx(self.thetaList[a], q)
+            self.qc.rz(self.thetaList[a+1], q)
+            self.qc2.rx(self.thetaList[a+2], q)
+            self.qc2.rz(self.thetaList[a+3], q)
+            a += 4
+        self.qc.barrier()
 
-            for i in range(4):
-                ent = self.qubits-1
-                if i == 0 :
-                    while ent > 0:
+        for i in range(4):
+            ent = self.qubits-1
+            if i == 0 :
+                while ent > 0:
+                    self.qc.crx(next(self.crxList), ent, ent-1)
+                    ent = ent -1  
+                self.qc.barrier()
+            if i == 1:
+                while ent > 0:
+                    if ent != self.qubits-1:
                         self.qc.crx(next(self.crxList), ent, ent-1)
-                        ent = ent -1  
-                    self.qc.barrier()
-                if i == 1:
-                    while ent > 0:
-                        if ent != self.qubits-1:
-                            self.qc.crx(next(self.crxList), ent, ent-1)
-                        else:
-                            self.qc.crx(next(self.crxList), ent-1, ent)
-                        ent = ent -1  
-                    self.qc.barrier()
-                if i == 2:
-                    while ent > 0:
-                        if ent != 1:
-                            self.qc.crx(next(self.crxList), ent-1, ent)
-                        else:
-                            self.qc.crx(next(self.crxList), ent, ent-1)
-                        ent = ent -1  
-                    self.qc.barrier()
-                if i == 3:
-                    while ent > 0:
-                        self.qc.crx(next(self.crxList), 0, ent)
-                        ent = ent -1  
-                    self.qc.barrier()
+                    else:
+                        self.qc.crx(next(self.crxList), ent-1, ent)
+                    ent = ent -1  
+                self.qc.barrier()
+            if i == 2:
+                while ent > 0:
+                    if ent != 1:
+                        self.qc.crx(next(self.crxList), ent-1, ent)
+                    else:
+                        self.qc.crx(next(self.crxList), ent, ent-1)
+                    ent = ent -1  
+                self.qc.barrier()
+            if i == 3:
+                while ent > 0:
+                    self.qc.crx(next(self.crxList), 0, ent)
+                    ent = ent -1  
+                self.qc.barrier()
+        self.qc3 = self.qc + self.qc2
 
-            
     def draw(self):
-        print(self.qc)
+        print(self.qc3)
 
 
-Circuit5(6,1, theta, theta).draw()
+Circuit5(9, theta, theta).draw()
 
 
